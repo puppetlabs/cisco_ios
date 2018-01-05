d629dfdbb3596035ec0011418aa1e39044a77902
Will Meek <william.meek@puppet.com>
2018-01-05 16:14:55 +0000

    (FM-6656) Adds full regular expression for populating the ntp server type

--- lib/puppet/provider/ntp_server/ios.rb
+++ lib/puppet/provider/ntp_server/ios.rb
@@ -2,6 +2,10 @@ require 'puppet/provider/cisco_ios'
 require 'pry'
 
 Puppet::Type.type(:ntp_server).provide(:rest, :parent => Puppet::Provider::Cisco_ios) do
+
+  @ntp_server_instance_regex = Regexp.new(%r{ntp server.+\n})
+  @ntp_server_value_regex = Regexp.new(%r{^.*ntp server (?<server_name>\S*)(?:(?: key )(?<key>\d+))?(?:(?: maxpoll )(?<maxpoll>\d+))?(?:(?: minpoll )(?<minpoll>\d+))?(?<prefer>( prefer)+)?(?:(?: source )(?<source>\S*))?})
+
   confine :feature => :posix
   defaultfor :feature => :posix
 
@@ -9,18 +13,21 @@ Puppet::Type.type(:ntp_server).provide(:rest, :parent => Puppet::Provider::Cisco
 
   def self.instances
     command = 'show running-config | include ntp server'
-    instance_regex = %r{ntp server.+\n}
-    value_regex = %r{^.*ntp server (?<server_name>\S*)}
     output = Puppet::Provider::Cisco_ios.run_command_enable_mode(command)
     return [] if output.nil?
 
-    raw_instances = output.scan(instance_regex)
+    raw_instances = output.scan(@ntp_server_instance_regex)
     new_instances = []
     raw_instances.each do |raw_instance|
-      value = raw_instance.match(value_regex)
+      value = raw_instance.match(@ntp_server_value_regex)
       new_instances << new(:name => value[:server_name],
-                              :ensure => :present,
-                             )
+                           :ensure => :present,
+                           :key => value[:key],
+                           :minpoll => value[:minpoll],
+                           :maxpoll => value[:maxpoll],
+                           :prefer => !value[:prefer].nil?,
+                           :source_interface => value[:source],
+                           )
     end
     new_instances
   end
