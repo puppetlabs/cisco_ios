feea40475a5113067319ee16de71d37b091b17d5
Will Meek <william.meek@puppet.com>
2018-01-08 14:42:21 +0000

    (FM-6656) Add function for setting supported fields of NTP server

--- lib/puppet/provider/ntp_server/ios.rb
+++ lib/puppet/provider/ntp_server/ios.rb
@@ -5,6 +5,7 @@ Puppet::Type.type(:ntp_server).provide(:rest, :parent => Puppet::Provider::Cisco
 
   @ntp_server_instance_regex = Regexp.new(%r{ntp server.+\n})
   @ntp_server_value_regex = Regexp.new(%r{^.*ntp server (?<server_name>\S*)(?:(?: key )(?<key>\d+))?(?:(?: maxpoll )(?<maxpoll>\d+))?(?:(?: minpoll )(?<minpoll>\d+))?(?<prefer>( prefer)+)?(?:(?: source )(?<source>\S*))?})
+  @ntp_server_config_string = nil
 
   confine :feature => :posix
   defaultfor :feature => :posix
@@ -33,23 +34,35 @@ Puppet::Type.type(:ntp_server).provide(:rest, :parent => Puppet::Provider::Cisco
   end
 
   def flush
-    # nothing happens at the minute
-    name = @property_hash[:name]
     if @property_hash[:ensure] == :absent
-      set_command = "no ntp server #{name}"
-      Puppet::Provider::Cisco_ios.run_command_conf_t_mode(set_command)
+      destroy
     else
-      set_command = "ntp server #{name}"
-      Puppet::Provider::Cisco_ios.run_command_conf_t_mode(set_command)
+      create
     end
+  end
+
+  def ntp_server_config_command
+    @ntp_server_config_string = '<state>ntp server <ip><key><minpoll><maxpoll><source><prefer>'
+    set_command = @ntp_server_config_string.gsub(/<state>/,(@property_hash[:ensure] == :absent) ? 'no ' : '')
+    set_command = set_command.to_s.gsub(/<ip>/, @property_hash[:name])
+    set_command = set_command.to_s.gsub(/<key>/, @property_hash[:key] ? " key #{@property_hash[:key]}" : '')
+    set_command = set_command.to_s.gsub(/<minpoll>/, @property_hash[:minpoll] ? " minpoll #{@property_hash[:minpoll]}" : '')
+    set_command = set_command.to_s.gsub(/<maxpoll>/, @property_hash[:maxpoll] ? " maxpoll #{@property_hash[:maxpoll]}" : '')
+    set_command = set_command.to_s.gsub(/<source>/, @property_hash[:source_interface] ? " source #{@property_hash[:source_interface]}" : '')
+    set_command = set_command.to_s.gsub(/<prefer>/, (@property_hash[:prefer] == :true) ? ' prefer' : '')
+    set_command
   end
 
   def create
     @create_elements = true
     @property_hash = resource.to_hash
+    Puppet::Provider::Cisco_ios.run_command_conf_t_mode(ntp_server_config_command)
   end
 
   def destroy
+    @property_hash = resource.to_hash
     @property_hash[:ensure] = :absent
+    Puppet::Provider::Cisco_ios.run_command_conf_t_mode(ntp_server_config_command)
   end
+
 end
