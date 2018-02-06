466b8e32a81b3a3d3338e76066e6a8faca4a53ac
Will Meek <william.meek@puppet.com>
2018-02-06 17:29:02 +0000

    (maint) Refactor of provider code

--- lib/puppet/provider/ntp_server/ntp_server.rb
+++ lib/puppet/provider/ntp_server/ntp_server.rb
@@ -1,53 +1,66 @@
 require 'puppet/resource_api'
+require 'puppet/resource_api/simple_provider'
 require 'puppet/util/network_device/cisco_ios/device'
-require 'puppet/provider/ntp_server/ntp_server_parse_utils'
+require 'puppet/utility'
 require 'pry'
 
 # NTP Server Puppet Provider for Cisco IOS devices
-class Puppet::Provider::NtpServer::NtpServer
-  def initialize; end
+class Puppet::Provider::NtpServer::NtpServer < Puppet::ResourceApi::SimpleProvider
+  def parse(output)
+    new_instance_fields = []
+    output.scan(%r{#{@commands_hash['default']['get_instances']}}).each do |raw_instance_fields|
+      name_field = raw_instance_fields.match(%r{#{@commands_hash['default']['name']['get_value']}})
+      key_field = raw_instance_fields.match(%r{#{@commands_hash['default']['key']['get_value']}})
+      minpoll_field = raw_instance_fields.match(%r{#{@commands_hash['default']['minpoll']['get_value']}})
+      maxpoll_field = raw_instance_fields.match(%r{#{@commands_hash['default']['maxpoll']['get_value']}})
+      prefer_field = raw_instance_fields.match(%r{#{@commands_hash['default']['prefer']['get_value']}})
+      source_field = raw_instance_fields.match(%r{#{@commands_hash['default']['source']['get_value']}})
+
+      new_instance = { name: name_field ? name_field[:name] : nil,
+                       ensure: :present,
+                       key: key_field ? key_field[:key] : nil,
+                       minpoll: minpoll_field ? minpoll_field[:minpoll] : nil,
+                       maxpoll: maxpoll_field ? maxpoll_field[:maxpoll] : nil,
+                       prefer: !prefer_field.nil?,
+                       source_interface: source_field ? source_field[:source] : nil }
+
+      new_instance.delete_if { |_k, v| v.nil? }
+
+      new_instance_fields << new_instance
+    end
+    new_instance_fields
+  end
+
+  def config_command(property_hash)
+    set_command = @commands_hash['default']['set_values']
+    set_command = set_command.gsub(%r{<state>}, (property_hash[:ensure] == :absent) ? 'no ' : '')
+    set_command = set_command.to_s.gsub(%r{<ip>}, property_hash[:name])
+    set_command = set_command.to_s.gsub(%r{<key>}, (property_hash[:key]) ? " key #{property_hash[:key]}" : '')
+    set_command = set_command.to_s.gsub(%r{<minpoll>}, (property_hash[:minpoll]) ? " minpoll #{property_hash[:minpoll]}" : '')
+    set_command = set_command.to_s.gsub(%r{<maxpoll>}, (property_hash[:maxpoll]) ? " maxpoll #{property_hash[:maxpoll]}" : '')
+    set_command = set_command.to_s.gsub(%r{<source>}, (property_hash[:source_interface]) ? " source #{property_hash[:source_interface]}" : '')
+    set_command.to_s.gsub(%r{<prefer>}, (property_hash[:prefer]) ? ' prefer' : '')
+  end
+
+  def initialize
+    @commands_hash = Puppet::Utility.load_yaml(File.expand_path(__dir__) + '/command.yaml')
+  end
 
   def get(_context)
     command = 'show running-config | section ntp server'
     output = Puppet::Util::NetworkDevice::Cisco_ios::Device.run_command_enable_mode(command)
     return [] if output.nil?
-    NTPServerParseUtils.parse(output)
-  end
-
-  def set(context, changes)
-    changes.each do |name, change|
-      is = change.key?(:is) ? change[:is] : (get(context) || []).find { |key| key[:id] == name }
-      should = change[:should]
-
-      is = { name: name, ensure: 'absent' } if is.nil?
-      should = { name: name, ensure: 'absent' } if should.nil?
-
-      if is[:ensure].to_s == 'absent' && should[:ensure].to_s == 'present'
-        context.creating(name) do
-          create(context, name, should)
-        end
-      elsif is[:ensure].to_s == 'present' && should[:ensure].to_s == 'present'
-        context.updating(name) do
-          update(context, name, should)
-        end
-      elsif is[:ensure].to_s == 'present' && should[:ensure].to_s == 'absent'
-        context.deleting(name) do
-          delete(context, name)
-        end
-      end
-    end
+    parse(output)
   end
 
   def create(_context, _name, should)
-    Puppet::Util::NetworkDevice::Cisco_ios::Device.run_command_conf_t_mode(NTPServerParseUtils.config_command(should))
+    Puppet::Util::NetworkDevice::Cisco_ios::Device.run_command_conf_t_mode(config_command(should))
   end
 
-  def update(_context, _name, should)
-    Puppet::Util::NetworkDevice::Cisco_ios::Device.run_command_conf_t_mode(NTPServerParseUtils.config_command(should))
-  end
+  alias update create
 
   def delete(_context, name)
     clear_hash = { name: name, ensure: :absent }
-    Puppet::Util::NetworkDevice::Cisco_ios::Device.run_command_conf_t_mode(NTPServerParseUtils.config_command(clear_hash))
+    Puppet::Util::NetworkDevice::Cisco_ios::Device.run_command_conf_t_mode(config_command(clear_hash))
   end
 end
