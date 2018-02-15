ed487b09519dc18edc8adcfc483f307ff88c4f0e
tphoney <tp@puppet.com>
2018-02-15 14:19:10 +0000

    (FM-6794) Implementing better edge case handling

--- lib/puppet/provider/syslog_settings/ios.rb
+++ lib/puppet/provider/syslog_settings/ios.rb
@@ -9,28 +9,22 @@ class Puppet::Provider::SyslogSettings::SyslogSettings < Puppet::ResourceApi::Si
   def parse_output(output)
     new_instance_fields = []
     name_value = 'default'
-    enable = output.match(%r{#{@commands_hash['default']['enable']['get_value']}}).nil?
-    expected_value = output.match(%r{#{@commands_hash['default']['console']['get_value']}})
-    console_string = if expected_value.nil?
-                       'informational'
-                     else
-                       expected_value[:console]
-                     end
-
-    expected_value = output.match(%r{#{@commands_hash['default']['monitor']['get_value']}})
-    monitor_string = if expected_value.nil?
-                       'informational'
-                     else
-                       expected_value[:monitor]
-                     end
-    source_interface = output.match(%r{#{@commands_hash['default']['source_interface']['get_value']}})[:source_interface]
-
+    enable_value = Puppet::Utility.parse_attribute(output, @commands_hash, 'enable')
+    console_string = Puppet::Utility.parse_attribute(output, @commands_hash, 'console')
+    monitor_string = Puppet::Utility.parse_attribute(output, @commands_hash, 'monitor')
+    source_interface = Puppet::Utility.parse_attribute(output, @commands_hash, 'source_interface')
+    time_stamp_units = Puppet::Utility.parse_attribute(output, @commands_hash, 'time_stamp_units')
+    vrf = Puppet::Utility.parse_attribute(output, @commands_hash, 'vrf')
     new_instance = { name: name_value,
-                     enable: enable,
+                     enable: Puppet::Utility.convert_no_to_boolean(enable_value),
                      monitor: Puppet::Utility.convert_level_name_to_int(monitor_string),
                      console: Puppet::Utility.convert_level_name_to_int(console_string),
                      source_interface: source_interface,
+                     time_stamp_units: time_stamp_units,
+                     vrf: vrf,
                      ensure: 'present' }
+
+    new_instance.delete_if { |_k, v| v.nil? }
 
     new_instance_fields << new_instance
     new_instance_fields
--- lib/puppet/utility.rb
+++ lib/puppet/utility.rb
@@ -19,6 +19,82 @@ class Puppet::Utility
     data_hash
   end
 
+  def self.version_match_ok
+    true
+  end
+
+  def self.safe_to_run(_exclusion_hash)
+    version_match_ok
+    # check device
+    true
+  end
+
+  def self.parse_attribute(output, command_hash, attribute)
+    # Is there a whole new device in the yaml at the top level
+    # eg
+    # ---
+    # default:
+    #  ...
+    # nxos:  <---- this is a device specific implementation
+    device_type = 'no_device_for_now'
+    parent_device = if command_hash[device_type].nil?
+                      'default'
+                    else
+                      # else use device specific yaml
+                      device_type
+                    end
+
+    # is there an device version of the attribute
+    attribute_device = if command_hash[parent_device]['attributes'][device_type].nil?
+                         'default'
+                       else
+                         device_type
+                       end
+    exclusion_hash = command_hash[parent_device]['attributes'][attribute][attribute_device]['excluded']
+    default_value = command_hash[parent_device]['attributes'][attribute][attribute_device]['default']
+    can_have_no_match = command_hash[parent_device]['attributes'][attribute][attribute_device]['can_have_no_match']
+    regex = command_hash[parent_device]['attributes'][attribute][attribute_device]['get_value']
+    if regex.nil?
+      Puppet.debug "Missing key/pair in yaml file for '#{attribute}'.\nExpects:#{parent_device}:->attributes:->#{attribute}:->#{attribute_device}:->get_value: 'regex here'"
+      returned_value = nil
+    else
+      returned_value = output.match(%r{#{regex}})
+    end
+
+    if safe_to_run(exclusion_hash)
+      if returned_value.nil?
+        # there is no match
+        if !can_have_no_match.nil?
+          # it is ok for this attribute to return nil
+          returny = returned_value
+        elsif !default_value.nil?
+          # use the default value
+          returny = default_value
+        else
+          Puppet.debug "Regex for attribute '#{attribute}' failed"
+        end
+      elsif returned_value.size > 1
+        # there is at least one match
+        returny = returned_value[attribute.to_sym]
+      else
+        # we dont have a nil or a match
+        raise 'Something is wrong if we hit this'
+      end
+    else
+      Puppet.debug "This attribute '#{attribute}', is not available for this device '' and/or version ''"
+    end
+    returny
+  end
+
+  def self.convert_no_to_boolean(value)
+    return_value = if value.nil?
+                     true
+                   else
+                     false
+                   end
+    return_value
+  end
+
   def self.convert_level_name_to_int(level_enum)
     level = if level_enum == 'debugging'
               7
