50f55b996d54560bc444963f9958ca9ba625488d
tphoney <tp@puppet.com>
2018-02-26 14:54:52 +0000

    (FM-6811) clean up for syslog_server

--- spec/unit/puppet/provider/syslog_server/syslog_server_spec.rb
+++ spec/unit/puppet/provider/syslog_server/syslog_server_spec.rb
@@ -1,47 +1,28 @@
 require 'spec_helper'
-require 'puppet/util/network_device/cisco_ios/device'
-# require 'puppet/resource_api/simple_provider'
-
-include RSpec::Mocks::ExampleMethods
 
 module Puppet::Provider::SyslogServer; end
 require 'puppet/provider/syslog_server/ios'
-require 'net/ssh/telnet'
 
-test_data = Puppet::Utility.load_yaml(File.expand_path(__dir__) + '/test_data.yaml', false)
-
-describe Puppet::Provider::SyslogServer::SyslogServer do
-  let(:provider) { described_class.new }
-  let(:device) { instance_double(Puppet::Util::NetworkDevice::Cisco_ios::Device, 'device') }
-  let(:transport) { instance_double(Puppet::Util::NetworkDevice::Transport::Cisco_ios, 'transport') }
-  let(:context) { instance_double('Puppet::ResourceApi::BaseContext', 'context') }
-  let(:connection) { instance_double(Net::SSH::Telnet, 'connection') }
-
-  before(:each) do
-    allow(Puppet::Util::NetworkDevice).to receive(:current).and_return(device)
-    allow(Puppet::Util::NetworkDevice::Cisco_ios::Device).to receive(:transport).and_return(transport)
-    allow(transport).to receive(:connection).and_return(connection)
-    allow(connection).to receive(:cmd).with(' ').and_return('cisco-c6503e#')
-    allow(connection).to receive(:cmd).with('String' => 'enable', 'Match' => %r{^Password:.*$|#})
-                                      .and_return('Password:')
-    allow(transport).to receive(:enable_password).and_return('test_pass')
-    allow(connection).to receive(:cmd).with('test_pass').and_return('cisco-c6503e#')
-    allow(connection).to receive(:cmd).with('show running-config all | section ^logging').and_return(device_output)
+RSpec.describe Puppet::Provider::SyslogServer::SyslogServer do
+  def self.load_test_data
+    Puppet::Utility.load_yaml(File.expand_path(__dir__) + '/test_data.yaml', false)
   end
 
-  test_data['default']['tests'].each do |test|
-    describe test['name'] do
-      let(:device_output) { test['device_output'] }
-      let(:expectations) do
-        expectations = []
-        test['expectations'].each do |x|
-          expectations.push eval(x) # rubocop:disable Security/Eval
-        end
-        expectations
-      end
+  let(:provider) { described_class.new }
+  let(:context) { instance_double('Puppet::ResourceApi::BaseContext', 'context') }
 
-      it':device_output parses to a puppet hash' do
-        expect(provider.get(context)).to eq expectations
+  context 'Read tests:' do
+    load_test_data['default']['read_tests'].each do |test_name, test|
+      it test_name.to_s do
+        expect(described_class.instances_from_cli(test['cli'])).to eq test['expectations']
+      end
+    end
+  end
+
+  context 'Update tests:' do
+    load_test_data['default']['update_tests'].each do |test_name, test|
+      it test_name.to_s do
+        expect(described_class.command_from_instance(test['instance'])).to eq test['cli']
       end
     end
   end
