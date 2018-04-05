7929994c69a7d32c715ec412b76177fc2e1992e2
tphoney <tp@puppet.com>
2018-04-05 13:46:20 +0100

    FM-6823 Implement radius

---
+++ lib/puppet/provider/radius/ios.rb
@@ -0,0 +1,45 @@
+require_relative '../../util/network_device/cisco_ios/device'
+require_relative '../../../puppet_x/puppetlabs/cisco_ios/utility'
+
+# Configure the domain name of the device
+class Puppet::Provider::Radius::Radius
+  def self.commands_hash
+    @commands_hash = PuppetX::CiscoIOS::Utility.load_yaml(File.expand_path(__dir__) + '/command.yaml')
+  end
+
+  def self.instances_from_cli(output)
+    new_instance_fields = []
+    new_instance = PuppetX::CiscoIOS::Utility.parse_resource(output, commands_hash)
+    new_instance[:name] = 'default'
+    new_instance.delete_if { |_k, v| v.nil? }
+    new_instance_fields << new_instance
+    new_instance_fields
+  end
+
+  def self.command_from_instance(instance)
+    PuppetX::CiscoIOS::Utility.build_commmands_from_attribute_set_values(instance, commands_hash)
+  end
+
+  def commands_hash
+    Puppet::Provider::Radius::Radius.commands_hash
+  end
+
+  def get(_context)
+    output = Puppet::Util::NetworkDevice::Cisco_ios::Device.run_command_enable_mode(PuppetX::CiscoIOS::Utility.get_values(commands_hash))
+    return [] if output.nil?
+    Puppet::Provider::Radius::Radius.instances_from_cli(output)
+  end
+
+  def set(context, changes)
+    changes.each do |name, change|
+      should = change[:should]
+      context.updating(name) do
+        update(context, name, should)
+      end
+    end
+  end
+
+  def update(_context, _name, should)
+    Puppet::Util::NetworkDevice::Cisco_ios::Device.run_command_conf_t_mode(Puppet::Provider::Radius::Radius.command_from_instance(should))
+  end
+end
