3e9d303695d8dea327fbd28e5edb73903e03df1b
tphoney <tp@puppet.com>
2018-04-11 17:07:04 +0100

    (FM-6822) adding initial attempt at port_channel

---
+++ lib/puppet/type/port_channel.rb
@@ -0,0 +1,145 @@
+require_relative '../../puppet_x/puppetlabs/netdev_stdlib/check'
+if PuppetX::NetdevStdlib::Check.use_old_netdev_type
+  Puppet::Type.newtype(:port_channel) do
+    @doc = 'Network Device Link Aggregation Group'
+
+    apply_to_all
+    ensurable
+
+    newparam(:name, namevar: true) do
+      desc 'LAG Name'
+
+      validate do |value|
+        case value
+        when String then super(value)
+        else raise "value #{value.inspect} is invalid, must be a String."
+        end
+      end
+    end
+
+    newparam(:force) do
+      desc 'Force configuration (true / false)'
+      newvalues(:true, :false)
+    end
+
+    newproperty(:id) do
+      desc 'Channel Group ID, e.g. 10'
+      munge { |v| Integer(v) }
+    end
+
+    newproperty(:description) do
+      desc 'Port Channel description'
+
+      validate do |value|
+        if value.is_a? String
+          super(value)
+          validate_features_per_value(value)
+        else raise "value #{value.inspect} is invalid, must be a string."
+        end
+      end
+    end
+
+    newproperty(:mode) do
+      desc 'LACP mode [ passive | active | disabled* ]'
+      newvalues(:active, :passive, :disabled)
+    end
+
+    newproperty(:interfaces, array_matching: :all) do
+      desc 'Array of Physical Interfaces'
+
+      validate do |val|
+        raise "value #{val.inspect} must be a string" unless val.is_a? String
+        raise "value #{val.inspect} has no digits" unless %r{\d+}.match(val) # rubocop:disable Performance/RedundantMatch
+      end
+
+      def insync?(is)
+        is.sort == @should.sort.map(&:to_s)
+      end
+    end
+
+    newproperty(:minimum_links) do
+      desc 'Number of active links required for LAG to be up'
+      munge { |v| Integer(v) }
+    end
+
+    newproperty(:speed) do
+      desc 'Link speed [auto*|10m|100m|1g|10g|40g|56g|100g]'
+      newvalues(:auto, '1g', '10g', '40g', '56g', '100g', '100m', '10m')
+    end
+
+    newproperty(:duplex) do
+      desc 'Duplex mode [auto*|full|half]'
+      newvalues(:auto, :full, :half)
+    end
+
+    newproperty(:flowcontrol_send) do
+      desc 'Flow control (send) [desired|on|off]'
+      newvalues(:desired, :on, :off)
+    end
+
+    newproperty(:flowcontrol_receive) do
+      desc 'Flow control (receive) [desired|on|off]'
+      newvalues(:desired, :on, :off)
+    end
+  end
+else
+  require 'puppet/resource_api'
+
+  Puppet::ResourceApi.register_type(
+    name: 'port_channel',
+    docs: 'Network Device Link Aggregation Group',
+    features: ['remote_resource'],
+    attributes: {
+      ensurable:      {
+        type:    'Optional[Boolean]', # PUP-2368 affects this
+        desc:    'Whether this network interface should be enabled on the target system.',
+        default: 'false',
+      },
+      name:     {
+        type:   'String',
+        desc:   'LAG Name',
+        behaviour: :namevar,
+      },
+      force:    {
+        type:   'Optional[Boolean]',
+        desc:   'Force configuration (true / false)',
+      },
+      id:    {
+        type:   'Optional[Integer]',
+        desc:   'Channel Group ID, e.g. 10',
+      },
+      description:    {
+        type:   'Optional[String]',
+        desc:   'Port Channel description',
+      },
+      mode:      {
+        type:    'Optional[String]',
+        desc:    'LACP mode [ passive | active | disabled* ]',
+      },
+      interfaces:      {
+        type:    'Optional[Array[String]]',
+        desc:    'Array of Physical Interfaces',
+      },
+      minimum_links:    {
+        type:   'Optional[Integer]',
+        desc:   'Number of active links required for LAG to be up',
+      },
+      speed:      {
+        type:    'Optional[String]',
+        desc:    'Link speed [auto*|10m|100m|1g|10g|40g|56g|100g]',
+      },
+      duplex:      {
+        type:    'Optional[String]',
+        desc:    'Duplex mode [auto*|full|half]',
+      },
+      flowcontrol_send:    {
+        type:   'Optional[String]',
+        desc:   'Flow control (send) [desired|on|off]',
+      },
+      flowcontrol_receive:    {
+        type:   'Optional[String]',
+        desc:   'Flow control (receive) [desired|on|off]',
+      },
+    },
+  )
+end
