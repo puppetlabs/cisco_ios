9859017c08fa66ba40e3b897e0b2a95010249a6e
tphoney <tp@puppet.com>
2018-05-02 16:11:40 +0100

    (FM-6921) network_vlan fix up

--- lib/puppet/provider/network_vlan/ios.rb
+++ lib/puppet/provider/network_vlan/ios.rb
@@ -1,8 +1,9 @@
+require 'puppet/resource_api/simple_provider'
 require_relative '../../util/network_device/cisco_ios/device'
 require_relative '../../../puppet_x/puppetlabs/cisco_ios/utility'
 
-# Network Interface Puppet Provider for Cisco IOS devices
-class Puppet::Provider::NetworkVlan::NetworkVlan
+# Network Vlan Puppet Provider for Cisco IOS devices
+class Puppet::Provider::NetworkVlan::NetworkVlan < Puppet::ResourceApi::SimpleProvider
   def self.commands_hash
     @commands_hash = PuppetX::CiscoIOS::Utility.load_yaml(File.expand_path(__dir__) + '/command.yaml')
   end
@@ -23,21 +24,21 @@ class Puppet::Provider::NetworkVlan::NetworkVlan
     new_instance_fields
   end
 
-  def self.commands_from_is_should(is, should)
+  def self.commands_from_instance(instance)
     array_of_commands = []
-    parent_device = 'default'
-    attributes_that_differ = (should.to_a - is.to_a).to_h
-    attributes_that_differ.each do |key, value|
-      next unless PuppetX::CiscoIOS::Utility.attribute_safe_to_run(commands_hash, key.to_s)
-      if key.to_s == 'ensure' && value.to_s == 'absent'
-        array_of_commands.push(PuppetX::CiscoIOS::Utility.convert_vlan_absent(commands_hash, should, parent_device))
-        return array_of_commands
-      elsif key.to_s == 'vlan_name'
-        set_command = PuppetX::CiscoIOS::Utility.convert_vlan_name(commands_hash, value, parent_device)
-      elsif key.to_s == 'shutdown'
-        set_command = PuppetX::CiscoIOS::Utility.convert_vlan_shutdown(commands_hash, value, parent_device)
-      end
-      array_of_commands.push(set_command) unless set_command.nil?
+    # if we create / delete only send a single command
+    if instance[:ensure] == 'absent'
+      array_of_commands.push(PuppetX::CiscoIOS::Utility.set_values(instance, commands_hash))
+    elsif instance[:ensure] == 'create'
+      instance[:ensure] = 'present'
+      array_of_commands.push(PuppetX::CiscoIOS::Utility.set_values(instance, commands_hash))
+    else
+      instance[:shutdown] = if instance[:shutdown] == false
+                              'no'
+                            else
+                              ''
+                            end
+      array_of_commands = PuppetX::CiscoIOS::Utility.build_commmands_from_attribute_set_values(instance, commands_hash)
     end
     array_of_commands
   end
@@ -52,25 +53,27 @@ class Puppet::Provider::NetworkVlan::NetworkVlan
     Puppet::Provider::NetworkVlan::NetworkVlan.instances_from_cli(output)
   end
 
-  def set(context, changes)
-    changes.each do |name, change|
-      is = change.key?(:is) ? change[:is] : (get(context) || []).find { |key| key[:name] == name }
-      should = change[:should]
-
-      context.updating(name) do
-        update(context, name, is, should)
-      end
-    end
-  end
-
-  def update(context, _name, is, should)
-    array_of_commands_to_run = Puppet::Provider::NetworkVlan::NetworkVlan.commands_from_is_should(is, should)
+  def update(context, name, should)
+    array_of_commands_to_run = Puppet::Provider::NetworkVlan::NetworkVlan.commands_from_instance(should)
     array_of_commands_to_run.each do |command|
-      context.device.run_command_vlan_mode(should[:name], command)
+      context.device.run_command_vlan_mode(name, command)
     end
   end
 
-  def create(context, _name, _should); end
+  def create(context, name, should)
+    create_hash = { name: name, ensure: 'create' }
+    array_of_commands_to_run = Puppet::Provider::NetworkVlan::NetworkVlan.commands_from_instance(create_hash)
+    array_of_commands_to_run.each do |command|
+      context.device.run_command_conf_t_mode(command)
+    end
+    update(context, name, should)
+  end
 
-  def delete(context, _name); end
+  def delete(context, name)
+    clear_hash = { name: name, ensure: 'absent' }
+    array_of_commands_to_run = Puppet::Provider::NetworkVlan::NetworkVlan.commands_from_instance(clear_hash)
+    array_of_commands_to_run.each do |command|
+      context.device.run_command_conf_t_mode(command)
+    end
+  end
 end
