9c721612611080f2a56a93c09acf40ab0e3cff18
Will Meek <william.meek@puppet.com>
2018-05-29 09:40:45 +0100

    (FM-6981) Add STP Global type and provider

--- examples/radius_server.pp
+++ examples/radius_server.pp
@@ -1,10 +1,10 @@
 radius_server { '2.2.2.2':
   ensure           => 'present',
   hostname         => '1.2.3.4',
-  auth_port        => '1642',
-  acct_port        => '1643',
+  auth_port        => 1642,
+  acct_port        => 1643,
   key              => 'bill',
-  key_format       => '1',
-  retransmit_count => '7',
-  timeout          => '42',
+  key_format       => 1,
+  retransmit_count => 7,
+  timeout          => 42,
 }
