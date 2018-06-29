22eca31c0c43fd6465d9b69229aa0405555a9113
Rick Sherman <rick@shermdog.com>
2018-06-29 16:15:43 -0500

    (NETDEV-41) add facility to syslog_settings

--- examples/syslog_settings.pp
+++ examples/syslog_settings.pp
@@ -1,5 +1,6 @@
 syslog_settings { 'default':
   enable           => false,
+  facility         => 'local5',
   monitor          => 6,
   console          => 6,
   source_interface => ['Loopback24'],
