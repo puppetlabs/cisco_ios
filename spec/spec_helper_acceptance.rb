ee71ece735e93218357a2dc163c6ec678c510afa
Lavinia Dan <lavinia.gergi@gmail.com>
2019-06-03 13:42:23 +0300

    FM-7849 missed call, needs debug arguments

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -36,7 +36,7 @@ end
 COMMON_ARGS = '--modulepath spec/fixtures/modules --deviceconfig spec/fixtures/acceptance-device.conf --target sut'.freeze
 
 def run_device(options = { allow_changes: true })
-  result = Open3.capture2e("bundle exec puppet device --apply #{@file.path} #{COMMON_ARGS}")
+  result = Open3.capture2e("bundle exec puppet device --apply #{@file.path} #{COMMON_ARGS} --verbose --trace --debug")
   if options[:allow_changes] == false
     expect(result[0]).not_to match(%r{^Notice: /Stage\[main\]})
   end
