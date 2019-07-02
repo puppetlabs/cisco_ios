8f736278e50242009521f5e742251afd5480d595
Dave Armstrong <david.armstrong@puppet.com>
2019-07-02 13:00:18 +0100

    (maint) Reorder network_trunk tests to avoid dynamic mode conflict

--- spec/acceptance/network_trunk_spec.rb
+++ spec/acceptance/network_trunk_spec.rb
@@ -5,24 +5,6 @@ describe 'network_trunk' do
     pp = <<-EOS
     network_trunk { 'Port-channel1':
       ensure => 'present',
-      mode => 'access',
-    }
-    EOS
-    make_site_pp(pp)
-    run_device(allow_changes: true)
-    # Are we idempotent
-    run_device(allow_changes: false)
-    # Check puppet resource
-    result = run_resource('network_trunk', 'Port-channel1')
-    expect(result).to match(%r{Port-channel1.*})
-    expect(result).to match(%r{mode.*access})
-    expect(result).to match(%r{ensure.*present})
-  end
-
-  it 'edit an existing trunk' do
-    pp = <<-EOS
-    network_trunk { 'Port-channel1':
-      ensure => 'present',
       encapsulation => 'dot1q',
       mode => 'dynamic_desirable',
       untagged_vlan => 42,
@@ -39,6 +21,24 @@ describe 'network_trunk' do
     expect(result).to match(%r{encapsulation.*dot1q}) if result =~ %r{encapsulation =>}
     expect(result).to match(%r{mode.*dynamic_desirable})
     expect(result).to match(%r{untagged_vlan.*42})
+    expect(result).to match(%r{ensure.*present})
+  end
+
+  it 'edit an existing trunk' do
+    pp = <<-EOS
+    network_trunk { 'Port-channel1':
+      ensure => 'present',
+      mode => 'access',
+    }
+    EOS
+    make_site_pp(pp)
+    run_device(allow_changes: true)
+    # Are we idempotent
+    run_device(allow_changes: false)
+    # Check puppet resource
+    result = run_resource('network_trunk', 'Port-channel1')
+    expect(result).to match(%r{Port-channel1.*})
+    expect(result).to match(%r{mode.*access})
     expect(result).to match(%r{ensure.*present})
   end
 
