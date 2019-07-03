de5e1bd2bbdb6d034fe8de425d4f26cb53cc835b
David Schmitt <david.schmitt@puppet.com>
2019-07-03 16:27:28 +0100

    (maint) clean up radius_server tests
    These tests should fail, not skip when the exclusion list is not correct.

--- spec/acceptance/radius_server_spec.rb
+++ spec/acceptance/radius_server_spec.rb
@@ -25,47 +25,42 @@ describe 'radius_server' do
     # Check puppet resource
     result = run_resource('radius_server', '2.2.2.2')
     # Does our target device support the 'new' Radius Server syntax?
-    # If so, should be present, otherwise we will skip test
-    if result =~ %r{ensure.*present}
-      expect(result).to match(%r{2.2.2.2.*})
-      # Has a key, encrypted by default on 2960
-      if result =~ %r{key_format.*7}
-        expect(result).to match(%r{key.*})
-      else
-        # Plaintext
-        expect(result).to match(%r{key.*bill})
-        expect(result).to match(%r{key_format.*1})
-      end
-      expect(result).to match(%r{hostname.*1.2.3.4})
-      expect(result).to match(%r{retransmit_count.*7})
-      expect(result).to match(%r{acct_port.*1643})
-      expect(result).to match(%r{auth_port.*1642})
-      expect(result).to match(%r{timeout.*42})
+    # If so, should be present, otherwise we will fail test and a human needs to revisit this
+    raise 'Radius server 2.2.2.2 not present, device might not be compatible' unless result =~ %r{ensure.*present}
+
+    expect(result).to match(%r{2.2.2.2.*})
+    # Has a key, encrypted by default on 2960
+    if result =~ %r{key_format.*7}
+      expect(result).to match(%r{key.*})
     else
-      skip 'Radius server 2.2.2.2 not present, device not compatible'
+      # Plaintext
+      expect(result).to match(%r{key.*bill})
+      expect(result).to match(%r{key_format.*1})
     end
+    expect(result).to match(%r{hostname.*1.2.3.4})
+    expect(result).to match(%r{retransmit_count.*7})
+    expect(result).to match(%r{acct_port.*1643})
+    expect(result).to match(%r{auth_port.*1642})
+    expect(result).to match(%r{timeout.*42})
   end
 
   it 'delete radius_server' do
     result = run_resource('radius_server', '2.2.2.2')
     # Does our target device support the 'new' Radius Server syntax?
-    # If so, should be present, otherwise we will skip test
-    if result =~ %r{ensure.*present}
-      pp = <<-EOS
-  radius_server { '2.2.2.2':
-    ensure => 'absent',
-  }
-      EOS
-      make_site_pp(pp)
-      run_device(allow_changes: true)
-      # Are we idempotent
-      run_device(allow_changes: false)
-      # Check puppet resource
-      result = run_resource('radius_server', '2.2.2.2')
-      expect(result).to match(%r{2.2.2.2.*})
-      expect(result).to match(%r{ensure.*absent})
-    else
-      skip 'Radius server 2.2.2.2 not present, device not compatible'
-    end
+    # If so, should be present, otherwise we will fail test and a human needs to revisit this
+    raise 'Radius server 2.2.2.2 not present, device might not be compatible' unless result =~ %r{ensure.*present}
+    pp = <<-EOS
+radius_server { '2.2.2.2':
+  ensure => 'absent',
+}
+    EOS
+    make_site_pp(pp)
+    run_device(allow_changes: true)
+    # Are we idempotent
+    run_device(allow_changes: false)
+    # Check puppet resource
+    result = run_resource('radius_server', '2.2.2.2')
+    expect(result).to match(%r{2.2.2.2.*})
+    expect(result).to match(%r{ensure.*absent})
   end
 end
