0ff64eb402c5bef2afe7bffadba7d27c8f8e8213
Max <max.magill@puppet.com>
2019-09-12 16:44:00 +0100

    Add vrf support to SNMP Notification Receiver
    Requested changes

--- spec/acceptance/snmp_notification_receiver_spec.rb
+++ spec/acceptance/snmp_notification_receiver_spec.rb
@@ -9,19 +9,6 @@ describe 'snmp_notification_receiver' do
       type => 'traps',
       ensure => 'present',
     }
-    EOS
-    make_site_pp(pp)
-    run_device(allow_changes: true)
-    # Are we idempotent
-    run_device(allow_changes: false)
-    # Check puppet resource
-    result = run_resource('snmp_notification_receiver', '"9.9.9.9 public 1234"')
-    expect(result).to match(%r{port.*1234})
-    expect(result).to match(%r{ensure.*present})
-  end
-
-  it 'add a different basic SNMP Notification Receiver' do
-    pp = <<-EOS
     snmp_notification_receiver { '9.9.9.9 public 5555':
       username => 'public',
       port => 5555,
@@ -33,9 +20,47 @@ describe 'snmp_notification_receiver' do
     # Are we idempotent
     run_device(allow_changes: false)
     # Check puppet resource
+    result = run_resource('snmp_notification_receiver', '"9.9.9.9 public 1234"')
+    expect(result).to match(%r{port.*1234})
+    expect(result).to match(%r{ensure.*present})
+
     result = run_resource('snmp_notification_receiver', '"9.9.9.9 public 5555"')
     expect(result).to match(%r{port.*5555})
     expect(result).to match(%r{ensure.*present})
+  end
+
+  it 'add a vrf SNMP Notification Receiver' do
+    skip "Test skip as #{device_model} does not support vrf" if ['2960', '4503'].include?(device_model)
+    pp = <<-EOS
+    snmp_notification_receiver { '8.8.8.8 public Mgmt-vrf 1235':
+      username => 'public',
+      vrf => 'Mgmt-vrf',
+      port => 1235,
+      type => 'traps',
+      ensure => 'present',
+    }
+    snmp_notification_receiver { '8.8.8.8 public Mgmt-vrf 6666':
+      username => 'public',
+      vrf => 'Mgmt-vrf',
+      port => 6666,
+      ensure => 'present',
+    }
+  EOS
+
+    make_site_pp(pp)
+    run_device(allow_changes: true)
+    # Are we idempotent
+    run_device(allow_changes: false)
+    # Check puppet resource
+    result_vrf = run_resource('snmp_notification_receiver', '"8.8.8.8 public Mgmt-vrf 1235"')
+    expect(result_vrf).to match(%r{port.*1235})
+    expect(result_vrf).to match(%r{ensure.*present})
+    expect(result_vrf).to match(%r{vrf.*Mgmt-vrf})
+
+    result_vrf = run_resource('snmp_notification_receiver', '"8.8.8.8 public Mgmt-vrf 6666"')
+    expect(result_vrf).to match(%r{port.*6666})
+    expect(result_vrf).to match(%r{ensure.*present})
+    expect(result_vrf).to match(%r{vrf.*Mgmt-vrf})
   end
 
   it 'remove an existing SNMP Notification Receiver' do
@@ -45,7 +70,13 @@ describe 'snmp_notification_receiver' do
       port => 1234,
       ensure => 'absent',
     }
+    snmp_notification_receiver { '9.9.9.9 public 5555':
+      username => 'public',
+      port => 5555,
+      ensure => 'absent',
+    }
     EOS
+
     make_site_pp(pp)
     run_device(allow_changes: true)
     # Are we idempotent
@@ -53,5 +84,36 @@ describe 'snmp_notification_receiver' do
     # Check puppet resource
     result = run_resource('snmp_notification_receiver', '"9.9.9.9 public 1234"')
     expect(result).to match(%r{ensure.*absent})
+    result = run_resource('snmp_notification_receiver', '"9.9.9.9 public 5555"')
+    expect(result).to match(%r{ensure.*absent})
+  end
+
+  it 'remove an existing vrf SNMP Notification receiver' do
+    skip "Test skip as #{device_model} does not support vrf" if ['2960', '4503'].include?(device_model)
+    pp = <<-EOS
+    snmp_notification_receiver { '8.8.8.8 public Mgmt-vrf 1235':
+      username => 'public',
+      vrf => 'Mgmt-vrf',
+      port => 1235,
+      type => 'traps',
+      ensure => 'absent',
+    }
+    snmp_notification_receiver { '8.8.8.8 public Mgmt-vrf 6666':
+      username => 'public',
+      vrf => 'Mgmt-vrf',
+      port => 6666,
+      ensure => 'absent',
+    }
+    EOS
+
+    make_site_pp(pp)
+    run_device(allow_changes: true)
+    # Are we idempotent
+    run_device(allow_changes: false)
+    # Check puppet resource
+    result_vrf = run_resource('snmp_notification_receiver', '"8.8.8.8 public Mgmt-vrf 1235"')
+    expect(result_vrf).to match(%r{ensure.*absent})
+    result_vrf = run_resource('snmp_notification_receiver', '"8.8.8.8 public Mgmt-vrf 6666"')
+    expect(result_vrf).to match(%r{ensure.*absent})
   end
 end
